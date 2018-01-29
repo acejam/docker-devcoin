2a3cb0f87de868766acc5650b9618fb80823524c
Joshua Noble <acejam@gmail.com>
2018-01-29 17:49:40 -0500

    Update logging and entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
-echo "server=1
-maxconnections=${MAX_CONNECTIONS}
-rpcuser=${RPC_USER}
-rpcpassword=${RPC_PASS}
-rpcallowip=${RPC_ALLOW_IP}
-rpcport=${RPC_PORT}
-port=${PORT}
-txindex=1" > /data/devcoin/devcoin.conf
+if [ ! -f /data/devcoin/devcoin.conf ]; then
+  echo "server=1
+  maxconnections=${MAX_CONNECTIONS}
+  rpcuser=${RPC_USER}
+  rpcpassword=${RPC_PASS}
+  rpcallowip=${RPC_ALLOW_IP}
+  rpcport=${RPC_PORT}
+  port=${PORT}
+  txindex=1" > /data/devcoin/devcoin.conf
+fi
 
 exec "$@"
